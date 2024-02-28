820753fd0512adfc7ea263d4357e109ea3d410d8
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-02-28 11:36:34 +0000

    use client-making counter component rendered on client side

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Click to count!</button>
+    </div>
+  );
+}
